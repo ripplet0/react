781ca4fde97d58fc5dff245ebf0aba37d1aa603a
tony001100 <100060984+tony001100@users.noreply.github.com>
2022-02-20 22:48:11 +0530

    Add files via upload

---
+++ react.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myelement = (
+  <ul>
+    <li>Kerala</li>
+    <li>Tamil Nadu</li>
+    <li>Karnataka</li>
+  </ul>
+);
+
+ReactDOM.render(myelement, document.getElementById('root'));
---
+++ react1.jsx
@@ -0,0 +1,12 @@
+mport React from 'react';
+import ReactDOM from 'react-dom';
+
+const x = 5;
+let text = "Goodbye";
+if (x < 10) {
+  text = "Hello";
+}
+
+const myelement = <h1>{text}</h1>;
+
+ReactDOM.render(myelement, document.getElementById('root'));
